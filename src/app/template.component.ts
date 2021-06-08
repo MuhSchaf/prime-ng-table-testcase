893d0874646a54974d9b9ccd71f3037d82448fac
MuhSchaf <benjamin_thiele@outlook.de>
2021-06-08 11:24:33 +0200

    refactor

--- src/app/template.component.ts
+++ src/app/template.component.ts
@@ -13,24 +13,14 @@ export class Person {
   templateUrl: './template.component.html',
   styleUrls: ['./template.component.css']
 })
-export class TemplateComponent implements OnInit {
+export class TemplateComponent {
   columnNames = ['nick', 'nickName'];
   useSorting = false;
-  cols: any[];
   dataSource: Person[] = [
     new Person('Alf', 'Gandalf', 'http://placekitten.com/200/300'),
     new Person('Rotbarsch', 'Blaumarsch', 'http://placekitten.com/200/200'),
     new Person('Marco', 'Buscha', 'https://www.placecage.com/200/300')
   ];
-
-  ngOnInit() {
-    this.cols = [
-      { field: 'name', header: 'Name' },
-      { field: 'nickName', header: 'Nickname' },
-      { field: 'imgSrc', header: 'Image' },
-      { field: 'color', header: 'Color' }
-    ];
-  }
 
   useSort(): void {
     this.useSorting = !this.useSorting;
